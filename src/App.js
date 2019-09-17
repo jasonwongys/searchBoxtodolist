3484db6eef576b3bd7b313b52648cf39922be8e1
Wong Yat Sing Jason  (NCS) <yat.sing.jason.wong@ncs.com.sg>
2019-09-17 15:53:06 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import List from './List'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list: [
+        "Go to the store",
+        "Wash the dishes",
+        "Learn some code"
+      ]
+    }
+
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+  removeItem(item) {
+    // Put our list into an array
+    const list = this.state.list.slice();
+
+    // Check to see if item passed in matches item in array
+    list.some((el,i) => {
+      if(el === item) {
+
+        // If item matches, remove it from array
+        list.splice(i, 1);
+        return true;
+      }
+    });
+
+    // Set state to list 
+    this.setState({
+      list: list
+    });
+  }
+
+  addItem(e) {
+
+    e.preventDefault();
+    let list = this.state.list;
+    const newItem = document.getElementById("addInput");
+    const form = document.getElementById("addItemForm");
+
+    if(newItem.value !="") {
+
+      list.push(newItem.value);
+
+      this.setState({
+        list: list
+      });
+
+      newItem.classList.remove("is-danger");
+      form.reset();
+    } else {
+      newItem.classList.add("is-danger");
+    }
+  }
+
+  render() {
+    return (
+      
+      <div className="content">
+      <h1> Search Box with To do List</h1>
+        <div className="container">
+          <section className="section">
+            <List items={this.state.list} delete={this.removeItem} />
+          </section>
+
+          <section className="section">
+            <form className="form" id="addItemForm">
+              <input
+                type="text"
+                className="input"
+                id="addInput"
+                placeholder="something that needs to be done"/>
+                
+                <button className="button is-info" onClick={this.addItem}> Add Item </button>
+            </form>
+          </section>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ src/List.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            filtered: []
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        let currentList = [];
+
+        let newList = [];
+
+        if(e.target.value !== "") {
+            currentList = this.props.items;
+
+            newList = currentList.filter(item => {
+                const lc = item.toLowerCase();
+                const filter = e.target.value.toLowerCase();
+
+                return lc.includes(filter);
+            });
+        } else {
+            newList = this.props.items;
+        }
+        this.setState({
+            filtered: newList
+        })
+    }
+
+    componentDidMount() {
+        this.setState({
+            filtered: this.props.items
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            filtered: nextProps.items
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <input onChange={this.handleChange} type="text" className="input" placeholder="Search.." />
+                <ul>
+                    {this.state.filtered.map(item => (
+                        <li key={item}>
+                            {item} &nbsp;
+                <span className="delete" onClick={() => this.props.delete(item)} /></li>
+                    ))}
+            </ul>
+            </div>
+        )
+    }
+}
+
+export default List
